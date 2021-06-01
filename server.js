dbfe4f3c75336958dc3f6c9ed6740d7756e6d1f7
fortega328 <fortega328@gmail.com>
2021-06-01 15:04:55 -0400

    changed file extension because of error

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(controllers);
 
 sequelize.sync({ force: false }).then(() => {
-    http.listen(PORT, () => console.log(`Listening on the coolest PORT ${PORT}`));
+    http.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 })
